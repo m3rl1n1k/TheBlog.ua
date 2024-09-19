166c2c9380e7544bce9593a8b74c685ef4ee1bdc
merl1n1k <bisix21@gmail.com>
2024-09-19 13:06:35 +0200

    add script for Unix system(easy access work with container)

---
+++ container.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+FILE=bulid.lock
+COMMAND=$1
+
+  if [[ $COMMAND == "start" ]]; then
+      docker compose --env-file .env.dev up;
+  fi
+  if [[ $COMMAND == "restart" ]]; then
+      docker compose --env-file .env.dev restart;
+  fi
+  if [[ $COMMAND == "stop" ]]; then
+      docker compose --env-file .env.dev stop $2;
+  fi
+  if [[ $COMMAND == "php" ]]; then
+      docker compose --env-file .env.dev exec -it "php" /bin/bash;
+      fi
+  if [[ $COMMAND == "build" ]]; then
+      docker compose --env-file .env.dev up --build;
+      fi
